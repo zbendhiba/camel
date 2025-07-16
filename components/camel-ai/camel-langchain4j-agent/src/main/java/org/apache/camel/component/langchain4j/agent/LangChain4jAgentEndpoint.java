be70e96dee268ad35207e6f4f85c271ce2ab1635
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2025-07-16 10:57:15 +0200

    Format + license

--- components/camel-ai/camel-langchain4j-agent/src/main/java/org/apache/camel/component/langchain4j/agent/LangChain4jAgentEndpoint.java
+++ components/camel-ai/camel-langchain4j-agent/src/main/java/org/apache/camel/component/langchain4j/agent/LangChain4jAgentEndpoint.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.langchain4j.agent;
 
 import org.apache.camel.Category;
@@ -5,17 +21,18 @@ import org.apache.camel.Component;
 import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
-import static org.apache.camel.component.langchain4j.agent.LangChain4jAgent.SCHEME;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
 import org.apache.camel.spi.UriPath;
 import org.apache.camel.support.DefaultEndpoint;
 
+import static org.apache.camel.component.langchain4j.agent.LangChain4jAgent.SCHEME;
+
 @UriEndpoint(firstVersion = "4.14.0", scheme = SCHEME,
-        title = "LangChain4j Agent",
-        syntax = "langchain4j-agent:agentId",
-        category = { Category.AI }, headersClass = LangChain4jAgent.Headers.class)
+             title = "LangChain4j Agent",
+             syntax = "langchain4j-agent:agentId",
+             category = { Category.AI }, headersClass = LangChain4jAgent.Headers.class)
 public class LangChain4jAgentEndpoint extends DefaultEndpoint {
     @Metadata(required = true)
     @UriPath(description = "The Agent id")
@@ -24,7 +41,8 @@ public class LangChain4jAgentEndpoint extends DefaultEndpoint {
     @UriParam
     private LangChain4jAgentConfiguration configuration;
 
-    public LangChain4jAgentEndpoint(String endpointUri, Component component, String agentId, LangChain4jAgentConfiguration configuration) {
+    public LangChain4jAgentEndpoint(String endpointUri, Component component, String agentId,
+                                    LangChain4jAgentConfiguration configuration) {
         super(endpointUri, component);
         this.agentId = agentId;
         this.configuration = configuration;
@@ -39,7 +57,6 @@ public class LangChain4jAgentEndpoint extends DefaultEndpoint {
     public Consumer createConsumer(Processor processor) throws Exception {
         return null;
     }
-
 
     /**
      * Return the Agent ID
